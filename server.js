27d7857d9554155c48e0762e8614fbbd8462236f
QLang0224 <157908068+QLang0224@users.noreply.github.com>
2024-04-13 16:52:29 -0600

    Update server.js

--- server.js
+++ server.js
@@ -183,7 +183,18 @@ const aggregate = [
     }
   }
 ];
-Movie.aggregate(aggregate).exec(function(err, doc) { ... });
+Movie.aggregate(aggregate).exec(function(err, doc) {Movie.aggregate(aggregate).exec(function(err, doc) {
+    if (err) {
+        res.status(500).json({ success: false, message: 'Failed to retrieve movie details.', error: err });
+    } else {
+        if (doc.length === 0) {
+            res.status(404).json({ success: false, message: 'Movie not found.' });
+        } else {
+            res.status(200).send({ success: true, movie: doc });
+        }
+    }
+});
+});
 
 router.post('/signin', function (req, res) {
     var userNew = new User();
