8921b27ab13421e8fb99c7789270c40b69e76241
QLang0224 <157908068+QLang0224@users.noreply.github.com>
2024-04-13 09:51:31 -0600

    Update Movies.js

--- Movies.js
+++ Movies.js
@@ -6,8 +6,16 @@ mongoose.connect(process.env.DB);
 const MovieSchema = new mongoose.Schema({
   title: { type: String, required: true, index: true },
   releaseDate: { type: Number, min: [1900, 'Must be greater than 1899'], max: [2100, 'Must be less than 2100']},
-  genre: { type: String, enum: genres },
-  actors: [ActorSchema],
+  genre: {
+    type: String,
+    enum: [
+      'Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Thriller', 'Western', 'Science Fiction'
+    ],
+  },
+   actors: [{
+    actorName: String,
+    characterName: String,
+  }],
   imageUrl: String,
 });
 
