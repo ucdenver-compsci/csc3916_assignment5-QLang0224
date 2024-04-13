b95b9725db24672f7d4a31b64855132ed13149dc
QLang0224 <157908068+QLang0224@users.noreply.github.com>
2024-04-13 17:18:56 -0600

    Update server.js

--- server.js
+++ server.js
@@ -151,7 +151,7 @@ router.get('/movies/:movieId', verifyToken, (req, res) => {
             if (doc.length === 0) {
                 res.status(404).json({ success: false, message: 'Movie not found.' });
             } else {
-                res.status(200).json({ success: true, movie: doc[0] }); 
+                res.status(200).send({ success: true, movie: doc[0] }); 
             }
         }
     });
